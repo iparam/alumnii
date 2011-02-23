683cb4700be4ff86465f61fc9b55a9f28c4d264b
Param <iparamtech@gmail.co>
2011-02-23 23:39:53 +0530

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alumni::Application.config.secret_token = 'be6e8a123f6f81116e16bd3b637f8d6a7a5fb97a2a19a78e6a8c949224fc5a696fcbccf943c8e49b1382b0ab3f846b6ba5b89844b5aa4b271fe459a9d5cf6bc9'
